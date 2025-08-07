adff1512f286bea049d2a3c6cf345a4dd6c84776
MSl <morozov.v_ob-122bt@mail.ru>
2025-08-07 23:49:46 +0700

    Снова Правка

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
-// frontend/vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // ← обязательно!
-  },
-});
+    outDir: 'build',
+  }
+})
